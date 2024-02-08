fd6dfb1a6cf764316f64705e590687f2028f3bab
MarcoGotti <mrc.gotti@gmail.com>
2024-02-08 18:39:32 +0000

    head commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <script>
+export default{
+  name: 'App',
+  data(){
+    return{
+      emails: []
+    }
+  },
+
+  mounted() {
+    for (let i = 0; i < 10; i++) { 
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {     
+      this.emails.push(response.data.response);      
+    })
+    }
+  }
+}
 
 </script>
 
 <template>
- 
+  
+  <ul v-if="emails.length == 10">
+    <li v-for="email in emails"><a href="#">{{ email }}</a></li> 
+  </ul>
+
 </template>
 
 <style>
+ul{
+  list-style: none;
 
+  & a{
+    color: white;
+  }
+}
 </style>
+
+
